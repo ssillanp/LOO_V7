8b9f2c74f66a8cc3e5b5b159b14031d7e269be27
sami <sami.sillanpaa@iki.fi>
2021-06-29 20:59:08 +0300

    V7T2 OK

--- app/src/main/java/com/example/loo_v7/MainActivity.java
+++ app/src/main/java/com/example/loo_v7/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.loo_v7;
 
 import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
     }
 
 
     public void testMethod(View v) {
-        System.out.println("Hello World!");
+        text.setText("Hello World!");
     }
 }
