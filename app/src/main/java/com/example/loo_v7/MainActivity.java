f9d85576de05a76456ffba745ab7fb6a35db23c1
sami <sami.sillanpaa@iki.fi>
2021-06-30 17:51:57 +0300

    V7T3 OK

--- app/src/main/java/com/example/loo_v7/MainActivity.java
+++ app/src/main/java/com/example/loo_v7/MainActivity.java
@@ -4,20 +4,26 @@ import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
+    TextInputEditText intext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.textView);
+        intext = (TextInputEditText) findViewById(R.id.textToUse);
     }
 
 
     public void testMethod(View v) {
-        text.setText("Hello World!");
+        System.out.println("Hello World!");
+        text.setText(intext.getText());
     }
 }
